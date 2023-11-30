a12ec77b37bd666461aabb0cfe41d72763c3bda2
M4rcoos <contato.shopvmax@gmail.com>
2023-11-30 10:16:47 -0300

    feat: add router to api

--- frontend/front/src/provider/party.ts
+++ frontend/front/src/provider/party.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const apiParty = axios.create({
-  baseURL: "http://localhost:3000/api/parties",
+  baseURL: "https://api-festa-26127d2de7a6.herokuapp.com/api/parties/",
   timeout: 1000,
 })
